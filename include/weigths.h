9c06b36635eefa044e1c8efe2237fd4278766a8c
CarlosCraveiro <carlos.craveiro@usp.br>
2024-06-05 22:13:13 -0300

    Adds a interface to operate feature data type

---
+++ include/weigths.h
@@ -0,0 +1,12 @@
+#ifdef __WEIGTHS_H__
+#define __WEIGTHS_H__
+
+typedef float feature_t; // Change to fixed-point representation in the future 
+
+/* Encapsulates operations to facilitate later changes */
+void multiply_feature(feature_t op1, feature_t op2, feature_t* result);
+void divide_feture(feature_t op1, feature_t op2, feature_t* result);
+void sum_feature(feature_t op1, feature_t op2, feature_t* result);
+void subtract_feature(feature_t op1, feature_t op2, feature_t* result);
+
+#endif /* __WEIGTHS_H__ */
---
+++ src/weights.c
@@ -0,0 +1,18 @@
+#include <weigths.h>
+
+void multiply_feature(feature_t op1, feature_t op2, feature_t* result) {
+    return op1 * op2;
+}
+
+void divide_feture(feature_t op1, feature_t op2, feature_t* result) {
+    return op1 / op2;
+}
+
+void sum_feature(feature_t op1, feature_t op2, feature_t* result) {
+    return op1 + op2;
+}
+
+void subtract_feature(feature_t op1, feature_t op2, feature_t* result) {
+    return op1 - op2;
+}
+

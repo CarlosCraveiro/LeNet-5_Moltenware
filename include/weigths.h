6d80863f467caaaf7ac1efc5d4ac19203eb28481
CarlosCraveiro <carlos.craveiro@usp.br>
2024-06-06 01:41:41 -0300

    adds feature type initializer function

--- include/weigths.h
+++ include/weigths.h
@@ -1,4 +1,4 @@
-#ifdef __WEIGTHS_H__
+#ifndef __WEIGTHS_H__
 #define __WEIGTHS_H__
 
 typedef float feature_t; // Change to fixed-point representation in the future 
@@ -8,5 +8,6 @@ void multiply_feature(feature_t op1, feature_t op2, feature_t* result);
 void divide_feture(feature_t op1, feature_t op2, feature_t* result);
 void sum_feature(feature_t op1, feature_t op2, feature_t* result);
 void subtract_feature(feature_t op1, feature_t op2, feature_t* result);
+void initialize_feature(feature_t* feat);
 
 #endif /* __WEIGTHS_H__ */

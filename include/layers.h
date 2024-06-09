ba0170941e5c26df2195042401afd04dc4618120
CarlosCraveiro <carlos.craveiro@usp.br>
2024-06-08 23:51:21 -0300

    Implements layer d6

--- include/layers.h
+++ include/layers.h
@@ -9,7 +9,7 @@ void layer_p2(feature_t* input, feature_t* output);
 void layer_c3(feature_t* input, feature_t* biases, feature_t* filters, feature_t* output);
 void layer_p4(feature_t* input, feature_t* output);
 void layer_d5(feature_t* input, feature_t* weights, feature_t* biases, feature_t* output);
-//void layer_d6(feature_t* input, feature_t* weights, feature_t* biases, feature_t* output);
+void layer_d6(feature_t* input, feature_t* weights, feature_t* biases, feature_t* output);
 //void layer_d7(feature_t* input, feature_t* weights, feature_t* biases, feature_t* output);
 
 #endif /* __LENET_5_LAYERS_H__ */
